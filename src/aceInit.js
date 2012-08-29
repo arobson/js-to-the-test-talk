e5a237762c1a61b894d05443e78d5ec4ba89c589
Alex Robson <asrobson@gmail.com>
2012-08-29 02:42:48 -0400

    Tweaking slides

--- src/aceInit.js
+++ src/aceInit.js
@@ -1,11 +1,22 @@
 $( function() {
-	$( "code" ).each( function( index, el ) {
-		var id = "editor-" + index;
-		el.id = id;
-		var editor = ace.edit( id );
-		editor.setTheme("ace/theme/textmate");
-		editor.session.setMode("ace/mode/javascript");
-		editor.setFontSize( "22px" );
-		editor.resize();
-	} );
+	// $( "code" ).each( function( index, el ) {
+	// 	var id = "editor-" + index;
+	// 	el.id = id;
+	// 	var editor = ace.edit( id );
+	// 	editor.setTheme("ace/theme/textmate");
+	// 	editor.session.setMode("ace/mode/javascript");
+	// 	editor.setFontSize( "22px" );
+	// 	editor.resize();
+	// } );
+
+	var el = $( "code" )[ 5 ]; //.each( function( index, el ) {
+	var id = "editor-" + 1;
+	el.id = id;
+	var editor = ace.edit( id );
+	editor.setTheme( "ace/theme/textmate" );
+	editor.session.setMode( "ace/mode/javascript" );
+	editor.setFontSize( "22px" );
+	editor.renderer.setHScrollBarAlwaysVisible( true );
+	editor.setAnimatedScroll( true );
+	editor.resize();
 } );
